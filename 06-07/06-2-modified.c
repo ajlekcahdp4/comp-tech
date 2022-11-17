e29312e73dc534c2b7f6a194e41df93618af0e0f
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-11-17 19:41:47 +0300

    complete seminar 6 and 7

--- 06-07/06-2-modified.c
+++ 06-07/06-2-modified.c
@@ -27,28 +27,29 @@ void *mythread(void *dummy)
 
 int main()
 {
-   pthread_t thid, mythid; 
+   pthread_t thid1, thid2, mythid; 
    int       result;
 
-   result = pthread_create( &thid, (pthread_attr_t *)NULL, mythread, NULL);
+   result = pthread_create( &thid1, (pthread_attr_t *)NULL, mythread, NULL);
    if(result != 0){
       printf ("Error on thread create, return value = %d\n", result);
       exit(-1);
    }   
-   printf("Thread created, thid = %u\n", thid);
+   printf("Thread created, thid = %u\n", thid1);
 
-   result = pthread_create( &thid, (pthread_attr_t *)NULL, mythread, NULL);
+   result = pthread_create( &thid2, (pthread_attr_t *)NULL, mythread, NULL);
    if(result != 0){
       printf ("Error on thread create, return value = %d\n", result);
       exit(-1);
    }   
-   printf("Thread created, thid = %u\n", thid);
+   printf("Thread created, thid = %u\n", thid2);
    mythid = pthread_self();
    a = a+1;
    
    printf("Thread %u, Calculation result = %d\n", mythid, a);
 
-   pthread_join(thid, (void **)NULL);
+   pthread_join(thid1, (void **)NULL);
+   pthread_join(thid2, (void **)NULL);
 
    return 0;
 }
